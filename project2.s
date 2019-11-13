2f78ebefdf216943f0f9cf1da9da5f993989b2a2
chuckluv <calovett35@gmail.com>
2019-11-13 04:09:43 -0500

    checks if unput is valid or not

--- project2.s
+++ project2.s
@@ -29,19 +29,21 @@ during:
 	la $t0,data
 	add $t0,$t0,$t1
 	lb $s0, ($t0)
-	bge $t2, 4, invalid
+	bge $t2, 5, invalid
 	bge $t3, 1, invalid
 	j sort
 sort:
-	beq $s0, $zero, finish
+	beq $s0, 0, finish
 	ble $s0, 47, special
 	ble $s0, 57, integer
 	ble $s0, 84, capital
 	ble $s0, 116, lowercase
 	bge $s0, 117, special
 special:
+	addi $t1,$t1, 1
 	beq $s0, 9,  gap
 	beq $s0, 32, gap
+	beq $s0, 10, finish
 	j invalid
 gap:
 	
@@ -49,31 +51,35 @@ gap:
 	j during			
 
 integer:
-	addi $t1, $t1, 1	
 	ble $s0, 47, special
+	addi $t1, $t1, 1
+	addi $t2, $t2, 1	
+	
 	li $t5, 48
 	sub $s0, $s0, $t5
 	add $s1, $s1, $s0
-	mult $t3, $t7	
-	mfhi $t3
+	mul $t3, $t3, $t7
 	j during
 capital:
-	addi $t1, $t1, 1
 	ble $s0, 64, special
+	addi $t1, $t1, 1
+	addi $t2, $t2, 1
+	
 	li $t5, 55
 	sub $s0, $s0, $t5
 	add $s1, $s1, $s0
-	mult $t3, $t7
-	mfhi $t3
+	mul $t3, $t3, $t7
+	
 	j during
 lowercase:
-	addi $t1, $t1, 1	
 	ble $s0, 96, special
+	addi $t1, $t1, 1
+	addi $t2, $t2, 1	
+	
 	li $t5, 87
 	sub $s0, $s0, $t5
 	add $s1, $s1, $s0
-	mult $t3, $t7
-	mfhi $t3
+	mul $t3, $t3, $t7
 	j during	
 
 finish:
@@ -84,7 +90,7 @@ finish:
 
 	li $v0, 1
 	
-	move $a0, $s1
+	move $a0, $t2
 	syscall
 	j Exit	
 	
@@ -93,8 +99,8 @@ invalid:
 	la $a0, output
 	syscall
 	
-	li $v0, 1
-	move $a0, $s1
+	li $v0, 4
+	la $a0, notvalid
 	syscall
 	j Exit
 
