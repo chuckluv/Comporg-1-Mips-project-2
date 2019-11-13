4015d7e2a06575861e426b6330971233a8fcb93c
chuckluv <calovett35@gmail.com>
2019-11-13 00:21:30 -0500

    check if the specail char is a tab or a space and check if there is a null

--- project2.s
+++ project2.s
@@ -16,6 +16,7 @@ before:
 	la $t0,data
 	add $t0,$t0,$t1
 	lb $s0, ($t0)
+	beq $s0, 0, finish
 	beq $s0, 9, skip
 	beq $s0, 32, skip
 	j during
@@ -27,16 +28,25 @@ during:
 	la $t0,data
 	add t0,$t0,$t1
 	lb $s0, ($t0)
-	bge $t2, 5, invalid
+	bge $t2, 4, invalid
+	bge $t3, 1, invalid
 	j sort
 sort:
+	beq $s0, $zero, finish
 	ble $s0, 47, special
 	ble $s0, 57, integer
 	ble $s0, 84, capital
 	ble $s0, 116, lowercase
 	bge $s0, 117, special
-		
-
+special:
+	beq $s0, 9,  gap
+	beq $s0, 32, gap
+	j invalid
+gap:
+	
+	addi $t3,$t3, -1
+	j during			
+	
 
 Exit:
 	li $v0, 10
